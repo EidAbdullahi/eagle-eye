e29872821fc8a9aad9e5bf0286242584edef6ede
EidAbdullahi <eidabdullahi10.com>
2025-08-18 12:39:55 +0300

    commit changes

--- myproject/urls.py
+++ myproject/urls.py
@@ -3,6 +3,12 @@ from django.urls import path
 from myapp import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect   # 👈 added
+
+# redirect view for /webmail
+def webmail_redirect(request):
+    # redirect users to the actual cPanel webmail login
+    return redirect("https://www.eagleyesecurityservice.com:2096")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -16,12 +22,10 @@ urlpatterns = [
     path('contact/', views.contact, name="contact"),
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
+    path('webmail/', webmail_redirect, name='webmail'),  # 👈 added
 ]
 
 # Only serve local media/static during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.ABOUT_IMAGES_URL, document_root=settings.ABOUT_IMAGES_ROOT)
-
-
-
